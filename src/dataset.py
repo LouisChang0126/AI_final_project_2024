34ec9acf4a50785e4f32131f55e64fbb31cd4e56
Sytwu <121273046+Sytwu@users.noreply.github.com>
2024-07-05 23:23:37 +0800

    update (resize images)

--- src/dataset.py
+++ src/dataset.py
@@ -12,10 +12,11 @@ import random
 
 def read_image(gray_img = False, coordinate = False):
     dataset = []
-    folder_paths = ['../data/1800~1970']
-    #folder_paths = ['../data/~1500', '../data/1400~1800', '../data/1800~1970', '../data/1970~']
+    #folder_paths = ['../data/1800~1970']
+    folder_paths = ['../data/~1500', '../data/1500~1800', '../data/1800~1970', '../data/1970~']
     for folder_path in folder_paths:
         floder_name = folder_path.split('/')[-1]
+        print(floder_name)
         df = pd.read_csv(f'../data/csv_data/{floder_name}.csv')
         i = 0
         for filename in df['picture']:
@@ -23,14 +24,14 @@ def read_image(gray_img = False, coordinate = False):
             i = i + 1
             filepath = os.path.join(folder_path, filename)
             if gray_img:
-                image_array = np.array(cv2.imread(filepath, cv2.IMREAD_GRAYSCALE))
+                image_array = np.array(cv2.resize(cv2.imread(filepath, cv2.IMREAD_GRAYSCALE),(256,256)))
             else:
-                image_array = np.array(cv2.imread(filepath))
+                image_array = np.array(cv2.resize(cv2.imread(filepath),(256,256)))
             if coordinate:
                 dataset.append((image_array, building_info['year'], building_info['longitude'], building_info['latitude']))
             else:
                 dataset.append((image_array, building_info['year']))
-            print(building_info['year'], building_info['longitude'], building_info['latitude'])
+            #print(building_info['year'], building_info['longitude'], building_info['latitude'])
     # train test split
     SPLIT_RATIO = 0.7
     random.shuffle(dataset)
